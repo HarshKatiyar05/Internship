89f43f52bc031a66692b928059ebef2d7f4d962d
Harsh Katiyar <hkatiyar370@gmail.com>
2026-07-13 19:37:33 +0530

    Question

---
+++ Internship.py/Hard_Question.java
@@ -0,0 +1,44 @@
+public class Hard_Question {
+  static  int peakElement(int arr[]){
+            int s=0;
+            int n = arr.length;
+            int e=n-1;
+            while(s<e){
+              int mid = s + (e-s)/2;
+              if(arr[mid]>arr[mid+1]){
+                    e = mid;
+              }else{
+                s=mid+1;
+              }
+            }
+            return e ;
+     }
+
+    static int OrderAgnostic_binarySearch(int arr[], int s, int e,int target){
+
+      boolean order = arr[s]<arr[e] ;
+
+      while(s<=e){
+          int mid = s+(e-s)/2 ;
+
+          if(target==arr[mid]){
+            return mid ;
+          }
+          if(order){
+            if(target<arr[mid]){
+               e = mid-1;
+            }else{
+              s= mid+1;
+            }
+          }else{
+            if(target>arr[mid]){
+              e = mid-1;
+            }else{
+              s = mid+1;
+            }
+          }
+      }
+      return -1;
+      
+    }
+}
